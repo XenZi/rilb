d1c05cebab8f6bfafbd9b64bd6dcfa2b35a43bf4
XenZi <rilakmateja@gmail.com>
2023-09-27 19:57:36 +0200

    feat: fix of _appDataSource

--- src/data-source/database.context.ts
+++ src/data-source/database.context.ts
@@ -1,21 +1,20 @@
 import { injectable } from "inversify";
 import { DataSource } from "typeorm";
 
-
 @injectable()
 export class Database {
-    private _appDataSource: DataSource = new DataSource({
-        type: "mysql",
-        host: "localhost",
-        port: 3306,
-        username: "root",
-        password: "root1234",
-        database: "rilb",
-        synchronize: true,
-        entities: [],
-    });
+  private _appDataSource: DataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "username here",
+    password: "password here",
+    database: "db name here",
+    synchronize: true,
+    entities: [],
+  });
 
-    get appDataSource() {
-        return this._appDataSource;
-    }
+  get appDataSource() {
+    return this._appDataSource;
+  }
 }
